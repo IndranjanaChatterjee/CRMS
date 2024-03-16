0a933efdce2078f8d115cc9aa7f998b9094c02ae
Indranjana Chatterjee <indranjanachatterjee@gmail.com>
2024-03-16 09:52:54 +0530

    Login

---
+++ login.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link rel="stylesheet" href="style2.css">
+</head>
+<body>
+    <div class="log">
+        <form action="" method="post">
+            <h1>LOGIN</h1>
+            <span class="name l">
+                <p>Username:</p>
+                <input type="text" name="name" placeholder="Enter Username">
+            </span>
+            <span class="password l">
+                <p>Password:</p>
+                <input type="password" name="name" placeholder="Enter Username">
+            </span>
+            <button>
+                <a href="#">Login</a>
+            </button>
+            <p>No account?Create account <a href="register.php">Register</a></p>
+        </form>
+    </div>
+</body>
+</html>
---
+++ register.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Register</title>
+    <link rel="stylesheet" href="style2.css">
+</head>
+<body>
+<div class="log">
+        <form action="" method="post">
+            <h1>REGISTER</h1>
+            <span class="name l">
+                <p>Username:</p>
+                <input type="text" name="name" placeholder="Enter Username">
+            </span>
+            <span class="password l">
+                <p>Password:</p>
+                <input type="password" name="password" placeholder="Enter Username">
+            </span>
+            <span class="address l">
+                <p>Address:</p>
+                <input type="text" name="address" placeholder="Enter Adress:">
+            </span>
+            <button>
+                <a href="#">Register</a>
+            </button>
+            <p class="last">Already an user ? <a href="login.php">Login</a></p>
+        </form>
+    </div>
+</body>
+</html>
